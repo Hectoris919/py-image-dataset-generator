39d1615cfcfbe4d6cece64ffdbd41687a9d9068e
Hectoris919 <47396668+Hectoris919@users.noreply.github.com>
2021-08-02 20:45:13 -0400

    Update google_grabber.py

--- image_grabber/google_grabber.py
+++ image_grabber/google_grabber.py
@@ -30,7 +30,7 @@ class GoogleGrabber(AbstractGrabber):
         
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
-#         options.add_argument('--no-sandbox')
+        options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
 
         browser = webdriver.Chrome(chrome_options=options)
